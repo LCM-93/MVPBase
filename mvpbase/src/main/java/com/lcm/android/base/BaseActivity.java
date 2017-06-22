65a41011afc0147365a81f5d6b9d1c974593e322
李晨鸣 <lichenming@link-pay.cn>
2017-06-22 19:07:21 +0800

    1、增加autoLayout

--- mvpbase/src/main/java/com/lcm/android/base/BaseActivity.java
+++ mvpbase/src/main/java/com/lcm/android/base/BaseActivity.java
@@ -3,9 +3,14 @@ package com.lcm.android.base;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
 
 
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
+import com.zhy.autolayout.AutoFrameLayout;
+import com.zhy.autolayout.AutoLinearLayout;
+import com.zhy.autolayout.AutoRelativeLayout;
 
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
@@ -23,6 +28,31 @@ public abstract class BaseActivity extends RxAppCompatActivity {
     protected BaseApplication mApplication;
     protected Context mContext;
     private Unbinder bind;
+
+    private static final String LAYOUT_LINEARLAYOUT = "LinearLayout";
+    private static final String LAYOUT_FRAMELAYOUT = "FrameLayout";
+    private static final String LAYOUT_RELATIVELAYOUT = "RelativeLayout";
+
+
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+        View view = null;
+        if (name.equals(LAYOUT_FRAMELAYOUT)) {
+            view = new AutoFrameLayout(context, attrs);
+        }
+
+        if (name.equals(LAYOUT_LINEARLAYOUT)) {
+            view = new AutoLinearLayout(context, attrs);
+        }
+
+        if (name.equals(LAYOUT_RELATIVELAYOUT)) {
+            view = new AutoRelativeLayout(context, attrs);
+        }
+
+        if (view != null) return view;
+
+        return super.onCreateView(name, context, attrs);
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
