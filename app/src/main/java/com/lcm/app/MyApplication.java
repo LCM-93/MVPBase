fa67c996929837049e0f5b89354c9159ca0bed0c
李晨鸣 <lichenming_hs@163.com>
2018-04-26 10:02:52 +0800

    更新支持 android studio 3.1

--- app/src/main/java/com/lcm/app/MyApplication.java
+++ app/src/main/java/com/lcm/app/MyApplication.java
@@ -28,10 +28,7 @@ public class MyApplication extends BaseApplication{
                 .build();
     }
 
-    @Override
-    protected String getBaseUrl() {
-        return "http://api.link-pay.cn/";
-    }
+
     /**
      * 将AppComponent返回出去,供其它地方使用, AppComponent接口中声明的方法返回的实例, 在getAppComponent()拿到对象后都可以直接使用
      *
